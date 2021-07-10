fa476dd7f28dab2d1468551c8a0a0f2446263268
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-07-10 15:23:49 +0100

    Add an easy producer function
    Easy is not simple

--- src/piotr_yuxuan/tsv_processing/user.clj
+++ src/piotr_yuxuan/tsv_processing/user.clj
@@ -1,7 +1,9 @@
 (ns piotr-yuxuan.tsv-processing.user
   "This file should better be `dev/user.clj` but for whatever reason I
   can't get IntelliJ to properly indent it."
-  (:require [piotr-yuxuan.tsv-processing.main :as main])
+  (:require [piotr-yuxuan.tsv-processing.main :as main]
+            [malli.generator :as mg]
+            [jackdaw.client :as jc])
   (:import (java.lang AutoCloseable)))
 
 (defonce app
@@ -24,3 +26,29 @@
   []
   (stop)
   (start))
+
+(defn producer
+  []
+  (let [producer-config {"bootstrap.servers" "localhost:9092"}
+        topic-config (:input-topic main/default-config)]
+    (with-open [client (jc/producer producer-config topic-config)]
+      (dotimes [_ 10e3]
+        (Thread/sleep 100)
+        (let [k (mg/generate string?)
+              v (mg/generate keyword?)]
+          @(jc/produce! client topic-config k v))))))
+
+(defonce producer-thread (atom nil))
+
+(comment
+  (reset! producer-thread
+          (Thread. ^Runnable
+                   (fn []
+                     (println :starting)
+                     (producer)
+                     (println :done))))
+  (.isAlive ^Thread @producer-thread)
+  (.start ^Thread @producer-thread)
+  (.isAlive ^Thread @producer-thread)
+  (.stop ^Thread @producer-thread)
+  (.isAlive ^Thread @producer-thread))
